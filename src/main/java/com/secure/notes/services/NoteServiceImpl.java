6134a634beb7b7e9278e6c5bc1fa5813d673cf6a
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-11-21 14:34:02 +0300

    implementing auditlog to noteService

--- src/main/java/com/secure/notes/services/NoteServiceImpl.java
+++ src/main/java/com/secure/notes/services/NoteServiceImpl.java
@@ -10,12 +10,14 @@ import java.util.List;
 public class NoteServiceImpl implements NoteService{
     @Autowired
     private NoteRepository noteRepository;
+    AuditLogService auditLogService;
     @Override
     public Note createNoteForUser(String username, String content) {
         Note note = new Note();
         note.setContent(content);
         note.setOwnerUsername(username);
         Note savedNote = noteRepository.save(note);
+        auditLogService.logNoteCreation(username,savedNote);
         return  savedNote;
     }
 
@@ -25,12 +27,17 @@ public class NoteServiceImpl implements NoteService{
                 -> new RuntimeException("Note not found"));
         note.setContent(content);
         Note updatedNote = noteRepository.save(note);
+        auditLogService.logNoteUpdate(username,updatedNote);
         return  updatedNote;
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
-        noteRepository.deleteById(noteId );
+        Note note = noteRepository.findById(noteId).orElseThrow(()->
+                new RuntimeException("Note not found"));
+        auditLogService.logNoteDeletion(username,noteId);
+        noteRepository.delete(note);
+
     }
 
     @Override
