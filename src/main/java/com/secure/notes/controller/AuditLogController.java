aab1c5e09f6c9e329376af7f4ec1a482578d054b
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-11-22 08:57:21 +0300

    authorize annation added to endpoints

--- src/main/java/com/secure/notes/controller/AuditLogController.java
+++ src/main/java/com/secure/notes/controller/AuditLogController.java
@@ -3,6 +3,7 @@ package com.secure.notes.controller;
 import com.secure.notes.entity.AuditLog;
 import com.secure.notes.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,12 @@ import java.util.List;
 public class AuditLogController {
     @Autowired
     AuditLogService auditLogService;
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping
     public List<AuditLog> getAuditLogs(){
         return auditLogService.getAllAuditLogs();
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/note/{id}")
     public List<AuditLog> getNoteAuditLogs(@PathVariable Long id){
         return auditLogService.getAuditLogsForNoteId(id);
